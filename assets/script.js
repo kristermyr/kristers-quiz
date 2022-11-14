b8fea365109cad5904ffffe5f9aa9fbd24b75304
kristermyr <krister90@gmail.com>
2022-11-14 14:27:42 -0600

    added forloop to have the other wrong buttons work. Have added funtions to the wrong buttons and added a 10 second penalty for choosing the wrong word.

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,35 @@
 var startButton = document.getElementById("start-btn");
 var questionContainerElement = document.getElementById("questions-container");
+const questionElement = document.getElementById("question")
+const answerBtnsElement = document.getElementById("answer-buttons")
 var timerElement = document.querySelector("#timer-count");
 var correctAnswer = document.getElementById("correct-btn");
-var wrongAnswer = document.querySelector(".wrong-btn");
+var question1 = document.getElementById("question1");
+var question2 = document.getElementById("question2");
+var question3 = document.getElementById("question3");
+var wrongButtons = document.querySelectorAll(".wrong-btn");
+var wrongButtons2 = document.querySelectorAll(".wrong-btn2");
+
+
+
+wrongButtons.forEach((btn) => {
+    return btn.addEventListener("click", (event) => {
+        wrongBtn();
+    });
+})
+wrongButtons2.forEach((btn) => {
+    return btn.addEventListener("click", (event) => {
+        wrongBtn2();
+    });
+})
+
 startButton.addEventListener("click", function() {
     startGame();
     startTimer();
 });
 
 
-var secondsLeft = 5;
+var secondsLeft = 90;
 var display =0;
 
   function startGame(){
@@ -29,4 +49,35 @@ function startTimer() {             //timer function
     }
 }, 1000);
     
+}
+correctAnswer.addEventListener("click", function() {    //correct answer button
+    
+    correctBtn();   
+});
+
+function correctBtn (){                  //correct answer function
+    
+    question1.classList.add("hide")
+    question2.classList.remove("hide")
+    console.log ("correct button")
+    secondsLeft = secondsLeft + 10;
+}   
+
+function wrongBtn() {
+
+    question1.classList.add("hide")
+    question2.classList.remove("hide")
+    secondsLeft = secondsLeft - 10;
+}
+function wrongBtn2() {
+    
+    question2.classList.add("hide")
+    question3.classList.remove("hide")
+        secondsLeft = secondsLeft - 10;
+        console.log ("wrong button 2");
+
+    }
+
+function nextQuestion(){
+
 }
